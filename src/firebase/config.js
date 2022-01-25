13952a4ed8ff2e18228752a822720b2c7e2bdbe3
usman arain <usmanarain7755@gmail.com>
2022-01-25 17:48:17 +0500

    whatsapp replica

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,10 +1,13 @@
 import Firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "",
-  databaseURL: "",
-  projectId: "",
-  appId: "",
+  databaseURL: "https://whatsappclone-8aea3-default-rtdb.firebaseio.com/",
+  apiKey: "AIzaSyB8eRqYCkLDhS9lepJx0653_2S5QoeQAN8",
+  authDomain: "whatsappclone-8aea3.firebaseapp.com",
+  projectId: "whatsappclone-8aea3",
+  storageBucket: "whatsappclone-8aea3.appspot.com",
+  messagingSenderId: "1007787787827",
+  appId: "1:1007787787827:web:5403192a6531a3cbc10546"
 };
 
 export default Firebase.initializeApp(firebaseConfig);
--- src/network/index.js
+++ src/network/index.js
@@ -1,8 +1,8 @@
-import LoginRequest from "./login";
-import SignUpRequest from "./signup";
+import LoginRequest from "./login/index";
+import SignUpRequest from "./signUp/index";
 import { AddUser, UpdateUser } from "./user";
-import LogOutUser from "./logout";
-import { senderMsg, recieverMsg } from "./messeges";
+import LogOutUser from "./logout/index";
+import { senderMsg, recieverMsg } from "./messeges/index";
 
 export {
   LoginRequest,
